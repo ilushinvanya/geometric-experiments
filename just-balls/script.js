2370bee1c7ed25b6c6fd3d93f1092b398e31842b
Иван Илюшин <iilyushin@samokat.ru>
2025-04-21 10:11:18 +0300

    удалил коллизии

--- just-balls/script.js
+++ just-balls/script.js
@@ -66,11 +66,8 @@ const cursor = new Cursor(false, 0,0);
 G = 9.8
 DeltaT = 0.1
 class Ball {
-	prevCollision = false
-	ballWithCollision = {}
 	constructor(newId, startX, startY, velocity, radian, symbol, symbolRadius) {
 		this.r = symbolRadius
-		this.id = newId
 		this.startX = startX
 		this.startY = startY
 		this.x = 0
@@ -80,7 +77,6 @@ class Ball {
 		this.radian = radian
 		this.symbol = symbol
 		this.currentVelocity = 0
-		this.currentRadian = 0
 		this.verticalCollapse = 0
 		this.horizontalCollapse = 0
 	}
@@ -91,7 +87,7 @@ class Ball {
 		// let collapse = 0
 		// if(this.verticalCollapse) verticalCollapse = 1
 		// if(this.horizontalCollapse) horizontalCollapse = 1
-		console.log(this.r - this.horizontalCollapse)
+		// console.log(this.r - this.horizontalCollapse)
 		// c.ellipse(this.x, this.y, this.r - this.horizontalCollapse, this.r - this.verticalCollapse,  Math.PI,0, Math.PI * 2)
 		c.ellipse(this.x, this.y, this.r, this.r - this.verticalCollapse,  Math.PI,0, Math.PI * 2)
 		if(this.verticalCollapse > 0 ) {
@@ -100,7 +96,7 @@ class Ball {
 		if(this.horizontalCollapse > 0 ) {
 			this.horizontalCollapse--
 		}
-		c.fillStyle = this.prevCollision ? 'red' : 'black'
+		c.fillStyle = 'black'
 		c.fill()
 		c.closePath()
 	}
@@ -120,8 +116,6 @@ class Ball {
 		const newY = this.y
 
 		const { angle, hypotenuse } = calcGeometry(oldX, oldY, newX, newY)
-		this.currentRadian = angle
-		this.currentHypotenuse = hypotenuse
 
 		this.currentVelocity = (hypotenuse / 1.4) / DeltaT
 
@@ -135,8 +129,6 @@ class Ball {
 			this.radian = Math.PI - angle
 			this.startY = this.y
 
-			console.log('rightSide', rightSide)
-			console.log('leftSide', leftSide)
 			if(rightSide)   this.startX = canvas.width - this.r
 			if(leftSide)    this.startX = this.r
 			this.horizontalCollapse = this.currentVelocity / 10
@@ -153,148 +145,12 @@ class Ball {
 			this.verticalCollapse = this.currentVelocity / 10
 		}
 
-		let prevNotCollisionT = 0
-		let prevNotCollisionTball = 0
-
-		// Столкновение мячиков между собой
-		const isCollision = balls.some(ball => {
-			if (ball.id === this.id) return false
-			const d = Math.sqrt((ball.x - this.x)**2 + (ball.y - this.y)**2)
-			if(d < this.r + ball.r) {
-				prevNotCollisionT = this.t - DeltaT
-				prevNotCollisionTball = ball.t - DeltaT
-				this.ballWithCollision = ball
-				return true
-			}
-		})
-		// isCollision - коллизия сейчас
-		// this.prevCollision - коллизия кадр назад
-		// не сейчас и не пред - ничего не делаем просто летим дальше
-		//
-
-		// не сейчас и пред - делаем баунс
-		if(!isCollision && this.prevCollision) {
-			this.bounce(this.ballWithCollision)
-			// this.prevCollision = isCollision
-			// this.draw()
-			// return
-		}
-
-		// сейчас и пред - ошибка, откатываемся еще
-		else if(isCollision && this.prevCollision) {
-			// this.t = this.t - 2 * DeltaT;
-			// this.update()
-			// return
-			this.t = 0
-			console.log(isCollision && this.prevCollision)
-		}
-
-		// сейчас и нет пред - откатываемся и перерисовываемся
-		else if(isCollision && !this.prevCollision) {
-			this.t = this.t - 2 * DeltaT;
-			// this.update()
-			// return
-		}
-		else if(!isCollision && !this.prevCollision) {
-			this.ballWithCollision = {}
-		}
-		this.prevCollision = isCollision
 		this.draw()
 	}
-	bounce(ball, newangle, prevNotCollisionTball, prevNotCollisionT) {
-		// this.draw()
-		this.t = 0
-		this.startVelocity = this.currentVelocity
-		this.radian = Math.PI - this.currentRadian
-		this.startY = this.y
-		this.startX = this.x
-		// this.horizontalCollapse = this.currentVelocity / 10
 
-		// ball.t = prevNotCollisionTball
-		// ball.draw()
-		ball.startVelocity = ball.currentVelocity
-		ball.t = 0
-		ball.radian = Math.PI - ball.currentRadian
-		ball.startY = ball.y
-		ball.startX = ball.x
-		// ball.horizontalCollapse = ball.currentVelocity / 10
-		return;
-		const m = this.r
-		const v1 = this.currentVelocity
-		const v2 = ball.currentVelocity
-		const t = this.t
-		// Рассчитайте импульсы шаров до столкновения
-		const p1 = m * v1;
-		const p2 = m * v2;
-
-		// Рассчитайте импульсы шаров после столкновения
-		const p1After = (p1 * (m - m) + 2 * m * p2) / (m + m);
-		const p2After = (p2 * (m - m) + 2 * m * p1) / (m + m);
-
-		// Рассчитайте скорости шаров после столкновения
-		const v1After = p1After / m;
-		const v2After = p2After / m;
-
-		// const theta = Math.atan2(v2_yBefore, v2_xBefore);
-		const v2_xBefore = v1 * Math.cos(theta);
-		// Рассчитайте скорость второго шара по оси x
-
-		// Рассчитайте угол между вектором скорости второго шара и осью x
-
-
-		// Здесь theta - это угол между вектором скорости второго шара до столкновения и осью x, который можно рассчитать с помощью функции Math.atan2:
-		// Рассчитайте компоненты вектора скорости второго шара
-		const theta = Math.atan2(v2_yBefore, v2_xBefore);
-		const v2_x = v2After * Math.cos(theta);
-		const v2_y = v2After * Math.sin(theta);
-
-		// Рассчитайте угол, под которым шары движутся после столкновения
-		const alpha = Math.atan(v2_y / v2_x);
-
-
-		// где v2_xBefore и v2_yBefore - компоненты вектора скорости второго шара до столкновения.
-
-		// Рассчитайте угол, под которым шары движутся после столкновения
-		const angle = Math.atan(v2_y / v2_x);
-
-		// Рассчитайте траектории движения шаров
-		const trajectory1 = {
-			x: this.x,
-			y: this.y,
-			vx: v1After * Math.cos(angle),
-			vy: v1After * Math.sin(angle) - G * t // учитываем ускорение свободного падения
-		};
-		const trajectory2 = {
-			x: ball.x,
-			y: ball.y,
-			vx: v2After * Math.cos(angle),
-			vy: v2After * Math.sin(angle) - G * t // учитываем ускорение свободного падения
-		};
-
-		// Рассчитайте новые траектории движения шаров с учетом движения по траектории
-		const newTrajectory1 = {
-			x: trajectory1.x + trajectory1.vx * t,
-			y: trajectory1.y + trajectory1.vy * t
-		};
-		const newTrajectory2 = {
-			x: trajectory2.x + trajectory2.vx * t,
-			y: trajectory2.y + trajectory2.vy * t
-		};
-
-		// Определите, пересекаются ли траектории движения шаров
-		const distance = Math.sqrt((newTrajectory1.x - newTrajectory2.x) ** 2 + (newTrajectory1.y - newTrajectory2.y) ** 2);
-		const radiusSum = this.r + ball.r;
-		const collision = distance <= radiusSum;
-
-		// Если траектории пересекаются, то шары столкнулись еще раз
-		if (collision) {
-			// Рассчитайте новые скорости и траектории движения шаров после второго столкновения
-			// Повторите шаги 2-6
-		}
-	}
-	destroy() {
-		balls = balls.filter(ball => ball.id !== this.id)
-	}
+	// destroy() {
+	// 	balls = balls.filter(ball => ball.id !== this.id)
+	// }
 }
 
 function animate() {
@@ -307,28 +163,3 @@ function animate() {
 }
 
 animate()
-// function motion(event){
-// 	console.log("Accelerometer: "
-// 		+ event.accelerationIncludingGravity.x + ", "
-// 		+ event.accelerationIncludingGravity.y + ", "
-// 		+ event.accelerationIncludingGravity.z
-// 	);
-// }
-// if(window.DeviceMotionEvent){
-// 	window.addEventListener("devicemotion", motion, false);
-// }else{
-// 	console.log("DeviceMotionEvent is not supported");
-// }
-// function orientation(event){
-// 	console.log("Magnetometer: "
-// 		+ event.alpha + ", "
-// 		+ event.beta + ", "
-// 		+ event.gamma
-// 	);
-// }
-// if(window.DeviceOrientationEvent){
-// 	DeviceOrientationEvent.requestPermission()
-// 	window.addEventListener("deviceorientation", orientation, false);
-// }else{
-// 	console.log("DeviceOrientationEvent is not supported");
-// }
